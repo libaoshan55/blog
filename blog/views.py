738dd7098f349e3b1820aadd21a75784649f4900
www_12 <wwwlibaoshan12@163.com>
2018-12-13 05:58:13 +0800

    2018年12月13日的提交

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 import markdown
-from .models import Post
+from comments.forms import CommentForm
+from .models import Post, Category
 
 """
 请使用下方的模板引擎方式。
@@ -36,4 +37,30 @@ def detail(request,pk):
                                       'markdown.extensions.codehilite',
                                       'markdown.extensions.toc',
                                   ])
-    return render(request,'blog/detail.html',context={'post':post})
+    form=CommentForm()
+    #获取这篇post下的所有评论
+    comment_list=post.comment_set.all()
+    # 将文章、表单、以及文章下的评论列表作为模板变量传给 detail.html 模板，以便渲染相应数据。
+    context={'post':post,
+             'form':form,
+             'comment_list':comment_list
+             }
+    return render(request,'blog/detail.html',context=context)
+
+#从数据库调用文章列表，筛选文章创建时间的年月
+def archives(request,year,month):
+    post_list=Post.objects.filter(
+        created_time__year=year,
+        created_time__month=month
+    )
+    # post_list = Post.objects.filter(
+    #     created_time__year=year,
+    #     created_time__month=month
+    # ).order_by("-created_time")
+    return render(request,'blog/index.html',context={'post_list':post_list})
+
+def category(request,pk):
+    #如果用户访问的分类不存在，则返回一个 404 错误页面以提示用户访问的资源不存在。
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
